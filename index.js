88ff36ef5fade53f1f1f6bda79ab1e1619517247
Ian Klug <ianklug123@gmail.com>
2023-02-25 17:56:07 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+class Node {
+    constructor(element)
+    {
+        this.element = element;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor()
+    {
+        this.head = null;
+        this.size = 0;
+    }
+    add(element){
+
+    }
+}
